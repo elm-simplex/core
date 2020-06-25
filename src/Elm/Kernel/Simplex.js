2a814a74aee6a5260b6296c434d3b4214897da3a
drathier <fh@concordium.com>
2020-06-26 00:35:11 +0200

    Flesh out placeholder js file

--- src/Elm/Kernel/Simplex.js
+++ src/Elm/Kernel/Simplex.js
@@ -1,1 +1,16 @@
-// intentionally empty
+/*
+
+*/
+
+// placeholder file
+
+var _Simplex_new = function(app)
+{
+	while(true){};
+});
+
+var _Simplex_migrateWithDowntime = function(app)
+{
+	while(true){};
+});
+
